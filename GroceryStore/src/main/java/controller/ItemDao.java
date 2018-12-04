e8b2242131b0d37335ac9a6c4e2781036f270930
jmpett <36994198+jmpett@users.noreply.github.com>
2018-12-03 18:06:12 -0600

    Newest Project

---
+++ GroceryStore/src/main/java/controller/ItemDao.java
@@ -0,0 +1,61 @@
+package controller;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import model.Item;
+
+public class ItemDao {
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("grocerystore");
+
+	public void cleanUp() {
+		emfactory.close();
+	}
+	
+	public void deleteItem(Item toDelete) { //<==== Needs looked at
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Item> typedQuery = em.createQuery("select i from Item i where i.item = :selectedItem and i.department = :selectedDepartment",Item.class);
+		typedQuery.setParameter("selectedItem", toDelete.getItem());
+		typedQuery.setParameter("selectedDepartment", toDelete.getDepartment());
+		typedQuery.setMaxResults(1);
+		Item result = typedQuery.getSingleResult();
+		em.remove(result);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public void insertItem(Item toInsert) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(toInsert);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public void updateItem(Item toEdit) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.merge(toEdit);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public List<Item> showAllItems() {
+		EntityManager em = emfactory.createEntityManager();
+		TypedQuery<Item> typedQuery = em.createQuery("select i from Item i", Item.class);
+		List<Item> foundTeams = typedQuery.getResultList();
+		em.close();
+		return foundTeams;
+	}
+	
+	public Item searchForItemByItemID(int id) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		Item found = em.find(Item.class, id);
+		em.close();
+		return found;
+	}
+}
---
+++ GroceryStore/src/main/java/controller/editServlet.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Item;
+
+@WebServlet("/editServlet")
+public class editServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public editServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ItemDao dao = new ItemDao();
+		String item = request.getParameter("item");
+		String deptID = request.getParameter("deptID");
+		String department = request.getParameter("department");
+		String price = request.getParameter("price");
+		Integer tempId = Integer.parseInt(request.getParameter("itemID"));
+		Item itemToUpdate = dao.searchForItemByItemID(tempId);
+		itemToUpdate.setItem(item);
+		itemToUpdate.setDeptID(deptID);
+		itemToUpdate.setDepartment(department);
+		itemToUpdate.setPrice(price);
+		dao.updateItem(itemToUpdate);
+		getServletContext().getRequestDispatcher("/viewAllServlet").forward(request, response);
+	}
+}
